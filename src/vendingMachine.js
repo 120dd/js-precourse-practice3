e420845144b30d329f406501fe7f08048605f268
120dd <cjs120dd@gmail.com>
2022-07-24 18:09:50 +0900

    feat:잔돈 반환하기 구현

--- src/vendingMachine.js
+++ src/vendingMachine.js
@@ -69,4 +69,32 @@ export class VendingMachine {
     findIndexByName(name, target){
         return target.findIndex(i=>i.name===name);
     }
+    
+    
+    returnChange(){
+        const balanceObj = {coins:this.balance};
+        let leftBalance = {...balanceObj}.coins;
+        const expect500 = Math.floor(leftBalance / CHANGE_COINS.COIN_500);
+        const have500 = this.changeCoins[0].quantity;
+        const returned500 = Math.min(expect500, have500);
+        leftBalance -= returned500 * CHANGE_COINS.COIN_500;
+        const expect100 = Math.floor(leftBalance / CHANGE_COINS.COIN_100);
+        const have100 = this.changeCoins[1].quantity;
+        const returned100 = Math.min(expect100, have100);
+        leftBalance -= returned100 * CHANGE_COINS.COIN_100;
+        const expect50 = Math.floor(leftBalance / CHANGE_COINS.COIN_50);
+        const have50 = this.changeCoins[2].quantity;
+        const returned50 = Math.min(expect50, have50);
+        leftBalance -= returned50 * CHANGE_COINS.COIN_50;
+        const expect10 = Math.floor(leftBalance / CHANGE_COINS.COIN_10);
+        const have10 = this.changeCoins[3].quantity;
+        const returned10 = Math.min(expect10, have10);
+        leftBalance -= returned10 * CHANGE_COINS.COIN_10;
+        this.balance = leftBalance;
+        this.changeCoins[0].quantity -= returned500;
+        this.changeCoins[1].quantity -= returned100;
+        this.changeCoins[2].quantity -= returned50;
+        this.changeCoins[3].quantity -= returned10;
+        return [returned500,returned100,returned50,returned10];
+    }
 }
