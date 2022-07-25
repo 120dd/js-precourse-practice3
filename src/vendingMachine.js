704a1b55555ffb72acbb05fb85fb6172d4945c61
120dd <cjs120dd@gmail.com>
2022-07-25 09:43:41 +0900

    refator: ui로직 삭제

--- src/vendingMachine.js
+++ src/vendingMachine.js
@@ -1,8 +1,5 @@
 import {Beverage} from "./beverage.js";
 import {CHANGE_COINS} from "./constants.js";
-import {Ui} from "./ui.js";
-
-const ui = new Ui();
 
 export class VendingMachine {
     constructor() {
@@ -14,11 +11,6 @@ export class VendingMachine {
             {'text': '50원', 'quantity': 0},
             {'text': '10원', 'quantity': 0},
         ]
-        this.initialize();
-    }
-    
-    initialize(){
-        ui.createTabButtons();
     }
 
     insertBalance(balance) {
@@ -31,7 +23,7 @@ export class VendingMachine {
     }
 
     /**
-     * 충전할 금액을 받아서 종류별 동전 개수를 충전
+     * 충전할 금액을 받아서 종류별 동전 개수를 랜덤하게 충전
      * @param {number} coins
      */
     chargeChangeCoins(coins) {
@@ -54,15 +46,14 @@ export class VendingMachine {
         const index = this.findIndexByName(beverageName, this.beverage);
         const targetBeverage = this.beverage[index];
         if (this.balance < targetBeverage.price ) {
-            ui.showAlert('잔액이 부족합니다');
-            return
+            return false;
         }
         if (targetBeverage.quantity < 1) {
-            ui.showAlert('품절입니다');
-            return
+            return false;
         }
         this.balance -= targetBeverage.price;
         targetBeverage.quantity--;
+        return true;
     }
 
     /**
