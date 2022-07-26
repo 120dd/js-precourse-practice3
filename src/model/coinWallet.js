229a20b531a871109ba1b435e85c09e9cfe35d5d
120dd <cjs120dd@gmail.com>
2022-07-26 16:16:35 +0900

    fix:코인 총 금액 확인하는 매소드 버그 수정

--- src/model/coinWallet.js
+++ src/model/coinWallet.js
@@ -3,16 +3,17 @@ import {COIN_TYPE} from "../constants.js";
 export class CoinWallet{
     constructor() {
         this.wallet = new Map([
-            [COIN_TYPE['500'],6],
-            [COIN_TYPE['100'],10],
-            [COIN_TYPE['50'],4],
-            [COIN_TYPE['10'],2],
+            [COIN_TYPE['500'],1],
+            [COIN_TYPE['100'],1],
+            [COIN_TYPE['50'],1],
+            [COIN_TYPE['10'],1],
         ]);
         this.totalBalance = this.getTotalBalance();
     }
 
     insertCoin(coinType, amount){
         this.wallet.set(COIN_TYPE[coinType],this.wallet.get(coinType)+amount);
+        this.totalBalance = this.getTotalBalance();
     }
 
     getTotalBalance(){
