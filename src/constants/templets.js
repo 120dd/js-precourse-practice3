b3385fddee82b52e1987164fcb358deeba8753c0
120dd <cjs120dd@gmail.com>
2022-08-01 15:35:22 +0900

    feat:상품관리, 잔돈 충전 탭 id 추가

--- src/constants/templets.js
+++ src/constants/templets.js
@@ -15,10 +15,12 @@ export const COMMON_HTML = `
 
 export const PRODUCT_MANAGE_TAB = `
 <h2>상품 추가하기</h2>
-<input id="product-name-input" type="text">
-<input id="product-price-input" type="number">
-<input id="product-quantity-input" type="number">
-<button id="product-add-button">추가하기</button>
+<form>
+    <input id="product-name-input" type="text">
+    <input id="product-price-input" type="number">
+    <input id="product-quantity-input" type="number">
+    <button id="product-add-button">추가하기</button>
+</form>
 <h2>상품 현황</h2>
 <table border="1">
 <tr>
@@ -26,34 +28,35 @@ export const PRODUCT_MANAGE_TAB = `
 <th>개수</th>
 <th>수량</th>
 </tr>
+<!--상품 추가될 때 아이디값 부여하기-->
 </table>
 `;
 
 export const CHARGE_COIN_TAB = `
 <h3>자판기 동전 충전하기</h3>
-<input type="number">
-<button>충전하기</button>
+<input type="number" id="vending-machine-charge-input">
+<button id="vending-machine-charge-button">충전하기</button>
 <h3>동전 보유 현황</h3>
-<table border="1">
+<table border="1" id="vending-machine-charge-amount">
     <tr>
         <th>동전</th>
         <th>개수</th>
     </tr>
     <tr>
         <th>500원</th>
-        <th></th>
+        <th id="vending-machine-coin-500-quantity"></th>
     </tr>
     <tr>
         <th>100원</th>
-        <th></th>
+        <th id="vending-machine-coin-100-quantity"></th>
     </tr>
     <tr>
         <th>50원</th>
-        <th></th>
+        <th id="vending-machine-coin-50-quantity"></th>
     </tr>
     <tr>
         <th>10원</th>
-        <th></th>
+        <th id="vending-machine-coin-10-quantity"></th>
     </tr>
 </table>
 `;
