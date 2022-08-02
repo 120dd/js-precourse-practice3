7abac0c51523e7b6947f599d63bc577a9189b62d
120dd <cjs120dd@gmail.com>
2022-08-02 14:21:36 +0900

    feat:SELECTOR 상수로 추가

---
+++ src/constants/constants.js
@@ -0,0 +1,26 @@
+export const TAB_BUTTON_IDS = {
+    PRODUCT_ADD_MENU_ID: 'product-add-menu',
+    VENDING_MACHINE_MANAGE_MENU_ID: 'vending-machine-manage-menu',
+    PRODUCT_PURCHASE_MENU: 'product-purchase-menu',
+}
+
+export const SELECTOR = {
+    COIN_MENU: "#vending-machine-manage-menu",
+    COIN_CHARGE_INPUT: "#vending-machine-charge-input",
+    COIN_CHARGE_BUTTON: "#vending-machine-charge-button",
+    COIN_500: "#vending-machine-coin-500-quantity",
+    COIN_100: "#vending-machine-coin-100-quantity",
+    COIN_50: "#vending-machine-coin-50-quantity",
+    COIN_10: "#vending-machine-coin-10-quantity",
+    PRODUCT_MENU: "#product-add-menu",
+    PRODUCT_NAME_INPUT: "#product-name-input",
+    PRODUCT_PRICE_INPUT: "#product-price-input",
+    PRODUCT_QUANTITY_INPUT: "#product-quantity-input",
+    PRODUCT_ADD_BUTTON: "#product-add-button",
+    PURCHASE_MENU: "#product-purchase-menu",
+    PURCHASE_CHARGE_INPUT: "#charge-input",
+    PURCHASE_CHARGE_AMOUNT: "#charge-amount",
+    PURCHASE_CHARGE_BUTTON: "#charge-button",
+    PURCHASE_ITEM_BUTTON: ".purchase-button",
+    PURCHASE_ITEM_QUANTITY: ".product-purchase-quantity",
+};
