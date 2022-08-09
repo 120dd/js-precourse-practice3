293c7276902b57bb0b5308b86d3315b4782f5ef1
120dd <cjs120dd@gmail.com>
2022-08-09 09:33:18 +0900

    test: purchaseProduct 내의 reduceUserBalance 함수의 테스트 추가

--- src/model/vendingMachine.test.js
+++ src/model/vendingMachine.test.js
@@ -11,10 +11,19 @@ window.localStorage = global.localStorage
 
 describe('vendingMachine.js', () => {
     describe('#purchaseProduct(index)', () => {
-        it('products 의 0번째 개수를 6개 감소시켰을 때 14를 반환해야한다', () => {
-            vendingMachine.products = [new Product('콜라',20,1200)];
-            vendingMachine.reduceProductQuantity(0, 6);
-            assert.ok(vendingMachine.products[0].quantity === 14);
+        describe('#reduceProductQuantity(index,quantity)', () => {
+            it('products 의 0번째 개수를 6개 감소시켰을 때 14를 반환해야한다', () => {
+                vendingMachine.products = [new Product('콜라',20,1200)];
+                vendingMachine.reduceProductQuantity(0, 6);
+                assert.ok(vendingMachine.products[0].quantity === 14);
+            });
+        });
+        describe('#reduceUserBalance(balance)', () => {
+            it('userBalance 가 3000일 때 1000을 감소 시키면 2000을 반환해야한다', () => {
+                vendingMachine.userBalance = 3000;
+                vendingMachine.reduceUserBalance(1000);
+                assert.ok(vendingMachine.userBalance === 2000);
+            });
         });
     });
 });
