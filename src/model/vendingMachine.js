bcd8adeae2560e846ddf96e16d397d42a7db26a0
120dd <cjs120dd@gmail.com>
2022-08-16 18:19:59 +0900

    refactor: DIP 원칙에 맞도록 vendingMachine 클래스가 추상화 모듈을 의존하도록 변경

--- src/model/vendingMachine.js
+++ src/model/vendingMachine.js
@@ -1,121 +1,125 @@
-import {Product} from "./product.js";
-import {COINS} from "../constants/constants.js";
-import {getLocalStorage, pickRandomNumInList, setLocalStorage} from "../utils/utils.js";
+import { Product } from './product.js';
+import { COINS } from '../constants/constants.js';
+import { pickRandomNumInList } from '../utils/utils.js';
+import { DataPersister } from './dataPersister.js';
 
 export class VendingMachine {
-    constructor() {
-        this.products = [];
-        this.machineCoins = [
-            {value: COINS.COIN_500, quantity: 0},
-            {value: COINS.COIN_100, quantity: 0},
-            {value: COINS.COIN_50, quantity: 0},
-            {value: COINS.COIN_10, quantity: 0},
-        ];
-        this.returnCoins = [
-            {value: COINS.COIN_500, quantity: 0},
-            {value: COINS.COIN_100, quantity: 0},
-            {value: COINS.COIN_50, quantity: 0},
-            {value: COINS.COIN_10, quantity: 0},
-        ];
-        this.userBalance = 0;
-        this.initProduct();
-        this.initCoin();
-        this.initBalance();
-    }
+	constructor() {
+		this.dataPersister = new DataPersister();
+		this.products = [];
+		this.machineCoins = [
+			{ value: COINS.COIN_500, quantity: 0 },
+			{ value: COINS.COIN_100, quantity: 0 },
+			{ value: COINS.COIN_50, quantity: 0 },
+			{ value: COINS.COIN_10, quantity: 0 },
+		];
+		this.returnCoins = [
+			{ value: COINS.COIN_500, quantity: 0 },
+			{ value: COINS.COIN_100, quantity: 0 },
+			{ value: COINS.COIN_50, quantity: 0 },
+			{ value: COINS.COIN_10, quantity: 0 },
+		];
+		this.userBalance = 0;
+		this.initProduct();
+		this.initCoin();
+		this.initBalance();
+	}
 
-    initProduct() {
-        const productDatas = getLocalStorage('productList');
-        if (productDatas) {
-            productDatas.forEach(productData => {
-                this.products.push(new Product(productData.name, Number(productData.quantity),
-                                               Number(productData.price)));
-            })
-        }
-    }
+	initProduct() {
+		const productDatas = this.dataPersister.getPersistData('productList');
+		if (productDatas) {
+			productDatas.forEach(productData => {
+				this.products.push(
+					new Product(productData.name, Number(productData.quantity), Number(productData.price)),
+				);
+			});
+		}
+	}
 
-    initCoin() {
-        if (getLocalStorage('machineCoin')) {
-            this.machineCoins = getLocalStorage('machineCoin');
-        }
-    }
+	initCoin() {
+		if (this.dataPersister.getPersistData('machineCoin')) {
+			this.machineCoins = this.dataPersister.getPersistData('machineCoin');
+		}
+	}
 
-    initBalance() {
-        if (getLocalStorage('userBalance')) {
-            this.userBalance = getLocalStorage('userBalance');
-        }
-    }
+	initBalance() {
+		if (this.dataPersister.getPersistData('userBalance')) {
+			this.userBalance = this.dataPersister.getPersistData('userBalance');
+		}
+	}
 
-    addProduct(name, price, quantity) {
-        const newProduct = new Product(name, quantity, price);
-        this.products.push(newProduct);
-        getLocalStorage('productList',this.products)
-    }
+	addProduct(name, price, quantity) {
+		const newProduct = new Product(name, quantity, price);
+		this.products.push(newProduct);
+		this.dataPersister.setPersistData('productList', this.products);
+	}
 
-    addMachineCoinRandomly(balance) {
-        let remainBalance = balance;
-        remainBalance = this.addRandomCoinList(remainBalance, COINS.COIN_500, [10, 50, 100, 500]);
-        remainBalance = this.addRandomCoinList(remainBalance, COINS.COIN_100, [10, 50, 100]);
-        remainBalance = this.addRandomCoinList(remainBalance, COINS.COIN_50, [10, 50]);
-        this.addRandomCoinList(remainBalance, COINS.COIN_10, [10]);
-    }
+	addMachineCoinRandomly(balance) {
+		let remainBalance = balance;
+		remainBalance = this.addRandomCoinList(remainBalance, COINS.COIN_500, [10, 50, 100, 500]);
+		remainBalance = this.addRandomCoinList(remainBalance, COINS.COIN_100, [10, 50, 100]);
+		remainBalance = this.addRandomCoinList(remainBalance, COINS.COIN_50, [10, 50]);
+		this.addRandomCoinList(remainBalance, COINS.COIN_10, [10]);
+	}
 
-    addRandomCoinList(remainBalance, coinValue, coinArray) {
-        while (remainBalance >= coinValue) {
-            const randomCoinValue = pickRandomNumInList(coinArray);
-            this.addMachineCoin(randomCoinValue);
-            remainBalance -= randomCoinValue;
-        }
-        return remainBalance;
-    }
+	addRandomCoinList(remainBalance, coinValue, coinArray) {
+		while (remainBalance >= coinValue) {
+			const randomCoinValue = pickRandomNumInList(coinArray);
+			this.addMachineCoin(randomCoinValue);
+			remainBalance -= randomCoinValue;
+		}
+		return remainBalance;
+	}
 
-    addMachineCoin(coinValue) {
-        const coinIndex = this.machineCoins.findIndex(coin => coin.value === coinValue);
-        this.machineCoins[coinIndex].quantity += 1;
-        setLocalStorage('machineCoin',this.machineCoins)
-    }
+	addMachineCoin(coinValue) {
+		const coinIndex = this.machineCoins.findIndex(coin => coin.value === coinValue);
+		this.machineCoins[coinIndex].quantity += 1;
+		this.dataPersister.setPersistData('machineCoin', this.machineCoins);
+	}
 
-    chargeUserBalance(balance) {
-        this.userBalance += Number(balance);
-        setLocalStorage('userBalance',this.userBalance)
-    }
+	chargeUserBalance(balance) {
+		this.userBalance += Number(balance);
+		this.dataPersister.setPersistData('userBalance', this.userBalance);
+	}
 
-    purchaseProduct(index) {
-        const newProducts = this.reduceProductQuantity(index,1);
-        this.reduceUserBalance(this.products[index].price);
-        setLocalStorage('productList',newProducts)
-    }
+	purchaseProduct(index) {
+		const newProducts = this.reduceProductQuantity(index, 1);
+		this.reduceUserBalance(this.products[index].price);
+		this.dataPersister.setPersistData('productList', newProducts);
+		this.dataPersister.setPersistData('userBalance', this.userBalance);
+	}
 
-    reduceProductQuantity(index,quantity){
-        this.products[index].quantity -= quantity;
-        return this.products;
-    }
+	reduceProductQuantity(index, quantity) {
+		this.products[index].quantity -= quantity;
+		return this.products;
+	}
 
-    reduceUserBalance(balance){
-        this.userBalance -= balance
-        return this.products;
-    }
+	reduceUserBalance(balance) {
+		this.userBalance -= balance;
+		return this.products;
+	}
 
-    returnChargeCoins() {
-        this.returnCoins.map((coin, idx) => {
-            if (this.userBalance >= coin.value) {
-                const expectQuantity = Math.floor(this.userBalance / coin.value);
-                const hasQuantity = this.machineCoins[idx].quantity;
-                coin.quantity += Math.min(expectQuantity, hasQuantity);
-                this.userBalance -= coin.quantity * coin.value;
-            }
-        });
-        this.machineCoins.map((currentCoin, idx) => {
-            currentCoin.quantity -= this.returnCoins[idx].quantity;
-        })
-        setLocalStorage('userBalance',this.userBalance);
-    }
+	returnChargeCoins() {
+		this.returnCoins.map((coin, idx) => {
+			if (this.userBalance >= coin.value) {
+				const expectQuantity = Math.floor(this.userBalance / coin.value);
+				const hasQuantity = this.machineCoins[idx].quantity;
+				coin.quantity += Math.min(expectQuantity, hasQuantity);
+				this.userBalance -= coin.quantity * coin.value;
+			}
+		});
+		this.machineCoins.map((currentCoin, idx) => {
+			currentCoin.quantity -= this.returnCoins[idx].quantity;
+		});
+		this.dataPersister.setPersistData('userBalance', this.userBalance);
+	}
 
-    resetReturnCoins() {
-        this.returnCoins = [
-            {value: COINS.COIN_500, quantity: 0},
-            {value: COINS.COIN_100, quantity: 0},
-            {value: COINS.COIN_50, quantity: 0},
-            {value: COINS.COIN_10, quantity: 0},
-        ];
-    }
+	resetReturnCoins() {
+		this.returnCoins = [
+			{ value: COINS.COIN_500, quantity: 0 },
+			{ value: COINS.COIN_100, quantity: 0 },
+			{ value: COINS.COIN_50, quantity: 0 },
+			{ value: COINS.COIN_10, quantity: 0 },
+		];
+	}
 }
