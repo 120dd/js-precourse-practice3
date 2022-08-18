1b38de9aa73cbe7ec60d30b9444f8fa5c0bb2a64
120dd <cjs120dd@gmail.com>
2022-08-18 13:33:57 +0900

    test: 코드 수정에 맞춰 테스트 코드 변경

--- src/model/vendingMachine.test.js
+++ src/model/vendingMachine.test.js
@@ -1,14 +1,13 @@
 import 'mock-local-storage';
-import { MachineData } from './machineData';
+import { LocalDataPersister } from '../DataPersister/LocalDataPersister';
 
 const assert = require('assert');
 const { VendingMachine } = require('./vendingMachine.js');
 const { Product } = require('./product.js');
 
-const vendingMachine = new VendingMachine();
-const machineData = new MachineData();
-machineData.products = [new Product('콜라', 20, 1200)];
-machineData.userBalance = 3000;
+const persister = new LocalDataPersister();
+const vendingMachine = new VendingMachine(persister);
+vendingMachine.products = [new Product('콜라', 20, 1200)];
 
 global.window = {};
 window.localStorage = global.localStorage;
@@ -17,14 +16,15 @@ describe('vendingMachine.js', () => {
 	describe('#purchaseProduct(index)', () => {
 		describe('#reduceProductQuantity(index,quantity)', () => {
 			it('products 의 0번째 개수를 6개 감소시켰을 때 14를 반환해야한다', () => {
-				machineData.reduceProductQuantity(0, 6);
-				assert.ok(machineData.products[0].quantity === 14);
+				vendingMachine.reduceProductQuantity(0, 6);
+				assert.ok(vendingMachine.products[0].quantity === 14);
 			});
 		});
 		describe('#reduceUserBalance(balance)', () => {
 			it('userBalance 가 3000일 때 1000을 감소 시키면 2000을 반환해야한다', () => {
-				machineData.reduceUserBalance(0);
-				assert.ok(machineData.userBalance === 1800);
+				vendingMachine.userBalance = 3000;
+				vendingMachine.reduceUserBalance(1000);
+				assert.ok(vendingMachine.userBalance === 2000);
 			});
 		});
 	});
